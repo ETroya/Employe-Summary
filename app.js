a17683e66dd028a6888485faa0a9192daa5c4258
Edwin Troya <edtroyajr@gmail.com>
2021-01-27 00:46:07 -0800

    moving files around

--- app.js
+++ app.js
@@ -6,29 +6,159 @@ const path = require("path");
 const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const outputpath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+const selectEmployee =[
+    {
+        message: `What position was hired on`,
+        type: `list`,
+        name: `type`,
+        choices: [`Engineer`,`Intern`, `Manager` ]
+    },
+]
+const addAnother = [
+    {
+        message: `Would you like to add another employee?`,
+        type: `confirm`,
+        name : `add`
+    },   
+]
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+function ask() {
+    inquirer.prompt(selectEmployee).then((answers)=>{
+     switch(answers.type){
+         case `manager`:
+             getManager();
+             break;
+         case `Engineer`:
+            getEngineer();
+            break;
+            default:
+                getIntern();    
+     }    
+    })
+};
+function asktoAdd(){
+    inquirer.prompt(addAnother).then((answers)=>{
+        if(answers.add){
+            ask();
+        }
+        else{
+        }
+    })
+}
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+function getManager() {
+    inquirer
+    .prompt([
+        {
+            message: `Enter First Name`,
+            type: "input",
+            name: "name"
+        },
+        {
+            message: `Enter ID number`,
+            type: "input",
+            name:"id"            
+        },
+        {
+            message:`Enter E-mail Address`,
+            type: "input",
+            name: `email`
+        },
+        {
+            message:`Enter Office Number`,
+            type: "input",
+            name: `officeNumber`
+        }
+    ])
+        .then((answers)=>{
+            const newManager = newManager(
+                answers.name,
+                answers.id,
+                answers.email,
+                answers.officerNumber,
+            )
+        
+        render(newManager)
+        asktoAdd();
+        })
+    }
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+function getEngineer() {
+    inquirer
+    .prompt([
+        {
+            message: `Enter First Name`,
+            type: "input",
+            name: "name"
+        },
+        {
+            message: `Enter ID number`,
+            type: "input",
+            name:"id"            
+        },
+        {
+            message:`Enter E-mail Address`,
+            type: "input",
+            name: `email`
+        },
+        {
+            message:`Enter GitHub`,
+            type: "input",
+            name: `github`
+        }
+    ])
+        .then((answers)=>{
+            const newEngineer = newEngineer(
+                answers.name,
+                answers.id,
+                answers.email,
+                answers.github,
+            )
+        
+        render(newEngineer)
+        asktoAdd();
+        })
+}
+function getIntern() {
+    inquirer
+    .prompt([
+        {
+            message: `Enter First Name`,
+            type: "input",
+            name: "name"
+        },
+        {
+            message: `Enter ID number`,
+            type: "input",
+            name:"id"            
+        },
+        {
+            message:`Enter E-mail Address`,
+            type: "input",
+            name: `email`
+        },
+        {
+            message:`Enter School Name`,
+            type: "input",
+            name: `school`
+        }
+    ])
+        .then((answers)=>{
+            const newIntern = new Intern(
+                answers.name,
+                answers.id,
+                answers.email,
+                answers.school,
+            )
+       
+        render(newIntern)
+        asktoAdd();
+        })
+}
+ask();
